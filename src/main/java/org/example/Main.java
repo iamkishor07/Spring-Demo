7296c02bc55032f18612e9e858ab8bb6a6805d59
Kishorkumar Dopathi <kishorkumardopathi16@gmail.com>
2023-01-25 00:32:35 +0530

    Spring annotations Demo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,20 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
+
+    // Spring core Annotations
+    //component
+    //Autowired
+    //Configuration
+    //primary used to mention if collsions occurs which need to execute first
+    //Component Scan is used to scan entire mentioned directory or path
+    //Quantifer used to mention the name which is going to execute if called
+
     public static void main(String[] args) {
         ApplicationContext factory=new AnnotationConfigApplicationContext(AppConfig.class);
 
         Teacher math=factory.getBean(Teacher.class);
+
         math.show();
 
     }
